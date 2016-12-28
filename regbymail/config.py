d87ac39b7ee6f55260a0e95079e50a850372addc
willgood1986 <willgood1986@outlook.com>
2016-12-28 12:54:08 +0800

    add requirement

--- regbymail/config.py
+++ regbymail/config.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 
-SQLALCHEMY_DATABASE_URI='mysql+pymysql://root:root@localhost:3306/mydb'
+SQLALCHEMY_DATABASE_URI='mysql+pymysql://:ROOT@localhost:3306/mydb'
 CSRF_ENABLED = True
 SECRET_KEY = "Should be serious for production"
 SECURITY_PASSWORD_SALT = 'MY_TREASURE'
