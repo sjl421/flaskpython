cbfd6b073a1f8139ee8aee8d683977273a2626c4
willgood1986 <willgood1986@outlook.com>
2017-02-28 07:40:12 -0800

    Passing an image to html using Flask

--- regbymail/regbymail/email.py
+++ regbymail/regbymail/email.py
@@ -2,8 +2,10 @@ from flask_mail import Message
 from . import mail, app
 
 def send_mail(to, subject, template):
-    message = Message(subject, recipients=[to], html=template, \
-        sender=app.config['MAIL_DEFAULT_SENDER'])
+    with mail.record_messages() as outbox:
+        message = Message(subject, recipients=[to], html=template, \
+            sender=app.config['MAIL_DEFAULT_SENDER'])
 
-    mail.send(message)
+        mail.send(message)
+        return len(outbox) == 1
 
--- regbymail/regbymail/testv/views.py
+++ regbymail/regbymail/testv/views.py
@@ -1,11 +1,47 @@
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, send_file, make_response
 from regbymail.ldebug import log
+
 
 testv_blueprint = Blueprint('testv', __name__)
 
 @testv_blueprint.route('/mainindex')
 def mainindex():
     try:
-        render_template('base.html')
+        log.log('Enter testv.mainindex')
+        return render_template('testv/showchart.html', chartid=1)
     except:
         log.log_exception()
+
+@testv_blueprint.route('/showchart/<chartid>')
+def showchart(chartid):
+    log.log('entert showchart')
+    log.log('charid is {}'.format(chartid))
+    import numpy as np
+    import matplotlib.pyplot as plt
+    from io import BytesIO
+    log.log('Enter testv blueprint')
+    N = 5
+    menMeans = (20, 35, 30, 35, 27)
+    womenMeans = (0, 32, 34, 20, 25)
+    menStd =(5, 8, 4, 1, 2)
+    womenStd=(1, 0, 1, 3, 3)
+    ind = np.arange(N)
+    width = 0.35
+    log.log('get all paras')
+    p1 = plt.bar(ind, menMeans, width, color='green', yerr=menStd)
+    p2 = plt.bar(ind, womenMeans, width, color='red', bottom=menMeans, yerr=womenStd)
+
+    plt.ylabel('Task Progress')
+    plt.title('Task progress group by item')
+    plt.xticks(ind, ('G1', 'G2', 'G3', 'G4', 'G5'))
+    plt.yticks(np.arange(0, 80, 10))
+    plt.legend((p1[0], p2[0]), ('Finished', 'UnFinished'))
+    log.log('try to save to image')
+
+    img = BytesIO()
+    log.log('new image instance')
+    plt.savefig(img, format='png')
+    img.seek(0)
+    log.log('save ok...')
+    log.log('before send image')
+    return send_file(img, mimetype='image/png')
